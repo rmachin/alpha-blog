77902bf46d190445cba1ff01189494f718263dfc
Raphael Machin <remachin75@gmail.com>
2018-02-09 10:07:21 -0500

    First commit - Practice rails navigation

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,10 @@
+class PageController < ActionController::Base
+  def home
+
+  end
+
+  def about
+    
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'page/home', to: 'page#home'
+  root 'page#home'
+  get 'about', to: 'page#about'
+end
